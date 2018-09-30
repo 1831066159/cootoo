8f67dfbf3ef5375913303f091eb8efc2c93cb98c
zhaoxiang <zhaoxiang@huli.com>
2018-09-30 11:19:19 +0800

    modify deploay to web container

--- ct-server-book/src/main/java/cn/com/cootoo/CtServerBookApplication.java
+++ ct-server-book/src/main/java/cn/com/cootoo/CtServerBookApplication.java
@@ -3,14 +3,22 @@ package cn.com.cootoo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
 @SpringBootApplication
 @ComponentScan
-public class CtServerBookApplication {
+public class CtServerBookApplication extends SpringBootServletInitializer {
+
 
     public static void main(String[] args) {
         SpringApplication.run(CtServerBookApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CtServerBookApplication.class);
     }
 }
