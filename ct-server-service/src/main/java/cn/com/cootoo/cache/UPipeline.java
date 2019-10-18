526375a5eec9c8c7c32e22f79e280a3bbf0d9623
zhaoxiang <zhaoxiang@huli.com>
2019-10-18 11:09:46 +0800

    update package

---
+++ ct-server-service/src/main/java/cn/com/cootoo/cache/UPipeline.java
@@ -0,0 +1,32 @@
+package cn.com.cootoo.cache;
+
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Pipeline;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author zhaoxiang
+ * @create 2019/6/27
+ **/
+public class UPipeline {
+
+
+    public static void usePipeline() {
+        String key = "key";
+        Jedis jedis = new Jedis("xx.xx.xx.xx");
+        Pipeline p = jedis.pipelined();
+        //要插入的数据列表
+        List<String> myData = new ArrayList<>();
+        for (String data : myData) {
+            p.hset("hh", "k_", data);
+        }
+        p.sync();
+        jedis.close();
+
+    }
+
+
+}
---
+++ ct-server-service/src/main/java/cn/com/cootoo/limit/RateLimiterDemo.java
@@ -0,0 +1,46 @@
+package cn.com.cootoo.limit;
+
+import com.google.common.util.concurrent.RateLimiter;
+
+/**
+ * @author zhaoxiang
+ * @create 2019/8/9
+ **/
+public class RateLimiterDemo {
+
+
+    public static void main(String[] args) {
+        testNoRateLimiter();
+        testWithRateLimiter();
+    }
+
+    public static void testNoRateLimiter() {
+        Long start = System.currentTimeMillis();
+        for (int i = 0; i < 10; i++) {
+            System.out.println(System.currentTimeMillis() + "-call execute.." + i);
+
+        }
+        Long end = System.currentTimeMillis();
+
+        System.out.println(end - start);
+
+    }
+
+    public static void testWithRateLimiter() {
+        Long start = System.currentTimeMillis();
+        // 每秒不超过1个任务被提交
+        RateLimiter limiter = RateLimiter.create(1.0);
+        for (int i = 0; i < 10; i++) {
+            // 请求RateLimiter, 超过permits会被阻塞
+            limiter.acquire();
+            System.out.println(System.currentTimeMillis() + "-call execute.." + i);
+
+        }
+        Long end = System.currentTimeMillis();
+
+        System.out.println(end - start);
+
+    }
+
+
+}
