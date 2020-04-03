3e9016f437d9756886e8ec95833e2be300c69ca5
zhaoxiang <zhaoxiang@huli.com>
2020-04-03 17:33:41 +0800

    add logback MDC test

---
+++ ct-server-service/src/main/java/cn/com/cootoo/log/LogbackMdc.java
@@ -0,0 +1,26 @@
+package cn.com.cootoo.log;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+
+/**
+ *
+ * Created by zhaoxiang
+ * Date 2020/4/3 5:30 下午
+ * Description
+ */
+@Slf4j
+public class LogbackMdc {
+
+    // logback 的MDC ，可以自定义日志字段, 在logback.xml 中使用 %X{myFlag} 打印
+
+    public static void main(String[] args) {
+        log.info("自定义日志字段myFlag");
+        MDC.put("myFlag","【FLAG_9】");
+
+        MDC.clear();//清除
+
+
+    }
+
+}
